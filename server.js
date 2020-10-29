4a94d416afa0e5e928b10d69de786e6dd6f439db
bdkelly <benjaminkelly97@gmail.com>
2020-10-28 23:08:05 -0400

    lab7

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 // These are our required libraries to make the server work.
 /* eslint-disable no-console */
 /* eslint-disable no-unused-vars */
-import countries from './public/lab_6/countries.js'
 import express from 'express';
+import dotenv from 'dotenv';
+import fetch from 'node-fetch';
+
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -11,22 +14,26 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.route('/api')
   .get((req, res) => {
     console.log('GET request detected');
+    
+    console.log('fetch data',json);
   })
-  .post((req, res) => {
+  .post(async (req, res) => {
     console.log('POST request detected');
+    const data = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
+    const json = await data.json();
     console.log('Form data in res.body', req.body);
-    res.json(countries);
+    res.json(json);
   });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
-
-});
-
-app.get('/',(req,res) => {
-  console.log("Hello World");
-  res.send("Hello World");
 });
