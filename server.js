272516b4171766bd9e2f52839a0671b40db1da9f
bdkelly <benjaminkelly97@gmail.com>
2020-11-18 14:12:38 -0500

    newer

--- server.js
+++ server.js
@@ -4,11 +4,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import fetch from 'node-fetch';
+/*import sqlite from 'sqlite';*/
+import sqlite3 from 'sqlite3';
 
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3002;
+const DB_PATH = ':memory:'
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -37,3 +40,44 @@ app.route('/api')
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
 });
+
+function databaseinit(){
+const dbSettings = {
+  filename:'./tmp/database.db',
+  driver: sqlite3.Database,
+};
+const DB = new sqlite3.Database(dbSettings.filename, function(err){
+  if (err) {
+      console.log(err)
+      return
+  }
+  console.log('Connected to ' + dbSettings.filename + ' database.')
+});
+  //
+  DB.exec('PRAGMA foreign_keys = ON;', function(error)  {
+    if (error){
+        console.error("Pragma statement didn't work.")
+    } else {
+        console.log("Foreign Key Enforcement is on.")
+    }
+  });
+  //
+  const dbschem = `CREATE TABLE IF NOT EXISTS Food(
+              id integer NOT NULL PRIMARY KEY,
+              name text NOT NULL,
+              category, 
+              inspection_date, 
+              inspection_results, 
+              city, 
+              state, 
+              zip, 
+              owner, 
+              type);`
+  DB.exec(dbschem,function(err){
+    if (err){
+      console.log(err)
+    }
+  });
+};
+
+databaseinit()
