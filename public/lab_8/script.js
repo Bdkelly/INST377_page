cfa717db7ded7ec33af79baa98bb84eed2c16fb6
Bdkelly <benjaminkelly97@gmail.com>
2020-11-28 12:57:15 -0500

    pretty good lab 8

--- public/lab_8/script.js
+++ public/lab_8/script.js
@@ -1,61 +1,50 @@
-function convertRestaurantsToCategories(restaurantList) {
-  // process your restaurants here!
-  return restaurantList.reduce((collection, item, i) => {
-    const findCat = collection.find((findItem) => findItem.label === item.category
-    );
-    if (!findCat) {
-      collection.push({
-        label: item.category,y: 1,});
-    } else {
-      findCat.y += 1;
-    }
-    return collection;
-  }, []);
-}
-
 function makeYourOptionsObject(datapointsFromRestaurantsList) {
   // set your chart configuration here!
-  CanvasJS.addColorSet('customColorSet1', [
-    // colorSet Array
-    "#2F4F4F",
-    "#008080",
-    "#2E8B57",
-    "#3CB371",
-    "#90EE90"
-  ]); // add an array of colors here https://canvasjs.com/docs/charts/chart-options/colorset/
-
-    return {
-      animationEnabled: true,
-      colorSet: 'customColorSet1',
-      title: {
-        text: 'Places To Eat Out In Future',
-      },
-      axisX: {
-        interval: 1,
-        labelFontSize: 12,
-      },
-      axisY2: {
-        interlacedColor: 'rgba(1,77,101,.2)',
-        gridColor: 'rgba(1,77,101,.1)',
-        title: 'Restaurants By Category',
-        labelFontSize: 12,
-        scaleBreaks: {
-          customBreaks: [
-            { startValue: 40, endValue: 50, color: '#EB8CC6' },
-            { startValue: 85, endValue: 100, color: '#3EA0DD' },
-            { startValue: 140, endValue: 175, color: '#EC5657' },
-          ],
-        }, // Add your scale breaks here https://canvasjs.com/docs/charts/chart-options/axisy/scale-breaks/custom-breaks/
-      },
-      data: [
-        {
-          type: 'bar',
-          name: 'restaurants',
-          axisYType: 'secondary',
-          dataPoints: datapointsFromRestaurantsList,
-        },
-      ],
-    };
+  return {
+    animationEnabled: true,
+    colorSet: 'customColorSet1',
+    title: {
+      text: 'Places To Eat Out In Future'
+    },
+    axisX: {
+      interval: 1,
+      labelFontSize: 12
+    },
+    axisY2: {
+      interlacedColor: 'rgba(1,77,101,.2)',
+      gridColor: 'rgba(1,77,101,.1)',
+      title: 'Restaurants By Category',
+      labelFontSize: 12,
+      scaleBreaks: {
+        customBreaks: [
+          {
+            startValue: 40,
+            endValue: 50,
+            color: 'orange',
+            type: 'zigzag'
+          },
+          {
+            startValue: 85,
+            endValue: 100,
+            color: 'orange',
+            type: 'zigzag'
+          },
+          {
+            startValue: 140,
+            endValue: 175,
+            color: 'orange',
+            type: 'zigzag'
+          }
+        ]
+      } // DONE -- Add your scale breaks here https://canvasjs.com/docs/charts/chart-options/axisy/scale-breaks/custom-breaks/
+    },
+    data: [{
+      type: 'bar',
+      name: 'restaurants',
+      axisYType: 'secondary',
+      dataPoints: datapointsFromRestaurantsList
+    }]
+  };
 }
 
 function runThisWithResultsFromServer(jsonFromServer) {
@@ -64,12 +53,21 @@ function runThisWithResultsFromServer(jsonFromServer) {
   // Process your restaurants list
   // Make a configuration object for your chart
   // Instantiate your chart
-  const newlistforresaurants = convertRestaurantsToCategories(jsonFromServer);
-  const catagories = makeYourOptionsObject(newlistforresaurants);
-  const elements = new CanvasJS.Chart("chartContainer",catagories);
-  elements.render();
+
+  CanvasJS.addColorSet('customColorSet1', [
+    '#C46A08',
+    '#3A1105',
+    '#F4AF28',
+    '#FADC7A'
+    // DONE -- add an array of colors here https://canvasjs.com/docs/charts/chart-options/colorset/
+  ]);
+
+  const options = makeYourOptionsObject(jsonFromServer);
+  const chart = new CanvasJS.Chart('chartContainer', options);
+  chart.render();
 }
 
+// Leave lines 52-67 alone; do your work in the functions above
 document.body.addEventListener('submit', async (e) => {
   e.preventDefault(); // this stops whatever the browser wanted to do itself.
   const form = $(e.target).serializeArray();
@@ -78,7 +76,11 @@ document.body.addEventListener('submit', async (e) => {
     headers: {
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify(form),
+    body: JSON.stringify(form)
   })
-    .then((fromSQL) => console.log(fromSQL));
+    .then((fromServer) => fromServer.json())
+    .then((jsonFromServer) => runThisWithResultsFromServer(jsonFromServer))
+    .catch((err) => {
+      console.log(err);
+    });
 });
--- server.js
+++ server.js
@@ -3,17 +3,19 @@
 /* eslint-disable no-unused-vars */
 import express from 'express';
 import dotenv from 'dotenv';
-
+import { open } from 'sqlite';
 import fetch from 'node-fetch';
 import sqlite3 from 'sqlite3';
-//import {databaseinit} from './sqlmaker.js';
 //
 dotenv.config();
+//
 const app = express();
 const port = process.env.PORT || 3002;
+//
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
+//
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
@@ -27,21 +29,48 @@ const dbSettings = {
   filename:'./tmp/database.db',
   driver: sqlite3.Database,
 };
-async function dataBaseStart(dbSettings){
+
+async function foodDataFetcher() {
+  const url = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
+  const data = await fetch(url);
+  return data.json()
+}
+//
+async function intodb(entry,db){
   try{
+    const store_name = entry.name;
+    const category = entry.category;
+    await db.exec(`INSERT INTO FoodDB(restaurant_name, category) VALUES ("$(store_name)","$(category)")`);
+    console.log(`${store_name} and ${category} inserted`);
+  }
+  catch(e) {
+		console.log('Error on insertion');
+		console.log(e);
+		}
+}
+///
+async function query(db){
+  const result = await db.all(`SELECT category, COUNT(restaurant_name) from FoodDB GROUP BY category`);
+  return result;
+}
+////
+async function dataBaseStart(dbSettings){
+  try {
     const db = await open(dbSettings);
     await db.exec(`CREATE TABLE IF NOT EXISTS FoodDB(
       id INTEGER PRIMARY KEY AUTOINCREMENT,
-      store_name TEXT,
+      restaurant_name TEXT,
       category TEXT)
-      `)
+      `);
+
     const data = await foodDataFetcher();
-    data.forEach((entry) => {intodb(entry)});
-    const test = await db.get('SELECT * FROM FoodDB')
-    console.log(test);
-  }
-  catch(e){
-    console.log("Error")
+
+    data.forEach((entry) => { intodb(entry, db); });
+
+    console.log('Database connected.');
+  } catch (e) {
+    console.log('Error loading Database.');
+    console.log(e);
   }
 }
 //
@@ -58,6 +87,9 @@ app.route('/api')
     res.json(json);
   });
 //
+
+dataBaseStart(dbSettings);
+
 app.route('/sql')
   .get((req, res) => {
     console.log('GET request detected');
@@ -66,31 +98,8 @@ app.route('/sql')
   .post(async (req, res) => {
     console.log('POST request detected');
     console.log('Form data in res.body', req.body);
-    const db = await dataBaseStart(dbSettings); //Creating DB
-    results = await query(db);
+    const db = await open(dbSettings); //Creating DB
+    const results = await query(db);
     res.json(results)
   });
 //
-async function foodDataFetcher() {
-  const url = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
-  const data = await fetch(url);
-  return data.json()
-}
-
-async function intodb(entry){
-  try{
-    const store_name = entry.name;
-    const category = entry.category;
-    await db.exec(`INSERT INTO FoodDB(store_name, category) VALUES ("$(store_name)","$(category)")`);
-    console.log(`${restaurant_name} and ${category} inserted`);
-  }
-  catch(e) {
-		console.log('Error on insertion');
-		console.log(e);
-		}
-}
-
-async function query(db){
-  const result = await db.all(`SELECT category, COUNT(store_name) from FoodDB GROUP BY category`);
-  return result;
-}
